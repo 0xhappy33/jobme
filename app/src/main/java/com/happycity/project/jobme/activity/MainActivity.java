19e0ccaf7bfb39b688f473032a871c4794d95b4b
hpcity <happy.city.community@gmail.com>
2017-10-05 15:16:04 +0700

    Setup firebase database

--- app/src/main/java/com/happycity/project/jobme/activity/MainActivity.java
+++ app/src/main/java/com/happycity/project/jobme/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.happycity.project.jobme;
+package com.happycity.project.jobme.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.happycity.project.jobme.R;
 
 public class MainActivity extends AppCompatActivity {
 
