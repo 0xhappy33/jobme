7a914e4b4e1934f05fb39bd2727312a0bbed1359
hpcity <happy.city.community@gmail.com>
2017-10-08 16:00:48 +0700

    Add ui login screen

---
+++ app/src/main/java/com/happycity/project/jobme/activity/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.happycity.project.jobme.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.happycity.project.jobme.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
